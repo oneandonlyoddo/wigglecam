392eca85c9d9280c2f8be7af7818cddf982732d0
SILVER\jonas <jonas.otto@field.io>
2026-02-23 10:16:46 +0100

    simplyfing and fixing the preview settings

--- camtest.py
+++ camtest.py
@@ -4,11 +4,9 @@ import time
 
 camera = Picamera2()
 
-WIDTH, HEIGHT = (800, 600)  # Set the desired width and height for the preview
-
 try:
-    camera.configure(camera.create_still_configuration())
-    camera.start_preview(Preview.DRM, x=0, y=0, width=WIDTH, height=HEIGHT)
+    camera.configure(camera.create_preview_configuration())
+    camera.start_preview(Preview.DRM)
     camera.start()
     while True:
         time.sleep(1)
