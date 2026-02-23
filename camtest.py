c1a247066fa729cf33cad95dbec297c310c790da
SILVER\jonas <jonas.otto@field.io>
2026-02-23 09:41:35 +0100

    fixing missing variables from copy and paste

--- camtest.py
+++ camtest.py
@@ -3,10 +3,11 @@ from picamera2 import Picamera2, Preview
 camera = Picamera2()
 camera.configure(camera.create_still_configuration())
 running = True
+WIDTH, HEIGHT = (800, 600)  # Set the desired width and height for the previw
 
 try:
     while running:
-      camera.start_preview(Preview.DRM, x=0, y=0, width=WIDTH, height=HEIGHT)
+      camera.start_preview(Preview.DRM, x=0, y=0, width=, height=HEIGHT)
       camera.start()
 except KeyboardInterrupt:
   running = False
