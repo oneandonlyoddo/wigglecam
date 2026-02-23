a11f9f36a7b95535893c65c0c31ec0adda350d84
SILVER\jonas <jonas.otto@field.io>
2026-02-23 09:59:30 +0100

    fix double event loop

--- camtest.py
+++ camtest.py
@@ -1,23 +1,24 @@
+#!/usr/bin/python3
 from picamera2 import Picamera2, Preview
+import time
 
 camera = Picamera2()
-camera.configure(camera.create_still_configuration())
-running = True
-WIDTH, HEIGHT = (800, 600)  # Set the desired width and height for the previw
+
+WIDTH, HEIGHT = (800, 600)  # Set the desired width and height for the preview
 
 try:
-    while running:
-      camera.start_preview(Preview.DRM, x=0, y=0, width=, height=HEIGHT)
-      camera.start()
+    camera.configure(camera.create_still_configuration())
+    camera.start_preview(Preview.DRM, x=0, y=0, width=WIDTH, height=HEIGHT)
+    camera.start()
+    while True:
+        time.sleep(1)
 except KeyboardInterrupt:
-  running = False
-  camera.stop_preview()
-  camera.stop()
-  camera.close()
-  print("Camera preview stopped gracefully.")
+    camera.stop_preview()
+    camera.stop()
+    camera.close()
+    print("Camera preview stopped gracefully.")
 except Exception as e:
-  camera.stop_preview()
-  camera.stop()
-  camera.close()
-  print(f"Error starting camera preview: {e}")
-    
+    camera.stop_preview()
+    camera.stop()
+    camera.close()
+    print(f"Error starting camera preview: {e}")
