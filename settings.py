9cc8d037619e0a1cfd5db2a4632b33c2206c09e1
SILVER\jonas <jonas.otto@field.io>
2026-02-24 01:10:37 +0100

    set trigger pin

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@
 # ============================================================================
 GPIO_SHUTTER_PIN = 26  # GPIO pin for shutter button (BCM numbering)
 GPIO_DEBOUNCE_TIME = 0.3  # Button debounce time in seconds
-GPIO_TRIGGER_PIN = 17  # GPIO pin for external trigger (BCM numbering)
+GPIO_TRIGGER_PIN = 27  # GPIO pin for external trigger (BCM numbering)
 # ============================================================================
