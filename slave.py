b007713419814433f5619e91a39c56f782b83001
SILVER\jonas <jonas.otto@field.io>
2026-02-24 01:20:37 +0100

    add flask server to send images after capture

--- slave.py
+++ slave.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+from flask import Flask, send_file
+import threading
 from picamera2 import Picamera2
 import RPi.GPIO as GPIO
 import os
@@ -5,11 +8,15 @@ from settings import *
 
 os.makedirs("./tmp", exist_ok=True)
 
+app = Flask(__name__)
 camera = Picamera2()
 camera.configure(camera.create_still_configuration())
 camera.start()
 
+latest_image = None
+
 def wait_for_trigger():
+    global latest_image
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(GPIO_TRIGGER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     
@@ -19,8 +26,14 @@ def wait_for_trigger():
         camera.capture_file(latest_image)
         print("Image captured!")
 
+@app.route('/get_image')
+def get_image():
+    if latest_image:
+        return send_file(latest_image, mimetype='image/jpeg')
+    return "No image yet", 404
+
+# Start trigger listener in background
+threading.Thread(target=wait_for_trigger, daemon=True).start()
+
 if __name__ == '__main__':
-    try:
-        wait_for_trigger()
-    finally:
-        GPIO.cleanup()
+    app.run(host='0.0.0.0', port=5000)
